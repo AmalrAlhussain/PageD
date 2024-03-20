509b6315f948b02a83e254ed2f552fef4edf5d3c
AmalRashed1 <160380379+AmalRashed1@users.noreply.github.com>
2024-03-20 10:51:26 +0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -16,12 +16,9 @@ import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
 
-app = dash.Dash(
-    title="testdoctor",
-    external_stylesheets=[dbc.themes.BOOTSTRAP],
-)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
 
 # Declare server for Heroku deployment. Needed for Procfile.
 server = app.server
